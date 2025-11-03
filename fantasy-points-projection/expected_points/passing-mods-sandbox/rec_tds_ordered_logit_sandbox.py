df0e2cd873576d8eed03a24d7d6719211da2309e
joshuafayallen <joshua.f.allen@gmail.com>
2025-11-03 13:00:30 -0500

    add cumulative stats

--- fantasy-points-projection/expected_points/passing-mods-sandbox/rec_tds_ordered_logit_sandbox.py
+++ fantasy-points-projection/expected_points/passing-mods-sandbox/rec_tds_ordered_logit_sandbox.py
@@ -38,6 +38,7 @@ clean_full_scores = full_scores.select(
         "home_score",
         "away_score",
         "home_team",
+        "total_line",
         "away_team",
         "result",
         "total",
@@ -57,7 +58,7 @@ rec_predictors = [
     "epa",
     "receiver_position",
     "surface",
-    "no_huddle",
+    # "no_huddle",
     "game_id",
     "yards_after_catch",
     "roof",
@@ -83,7 +84,7 @@ rec_data_full = (
         .over(["receiver_full_name", "game_id"])
         .alias("targeted"),
         pl.col("pass_attempt")
-        .sum()
+        .cum_sum()
         .over(["posteam", "game_id"])
         .alias("total_pass_attempts"),
     )
@@ -303,7 +304,6 @@ def_play_caller_idx = pd.Categorical(
 factors_numeric = [
     "player_rest_diff",
     "def_epa_diff",
-    #'receiver_epa_diff', # redundant but keeps the sampler happy at times decided ulitmately to remove
     "wind",
     "temp",
     "total_pass_attempts",
@@ -1511,32 +1511,338 @@ with pm.Model(coords=coords2) as rec_tds_era_adjusted:
 with rec_tds_era_adjusted:
     idata2 = pm.sample_prior_predictive()
 
-with rec_tds_mod:
-    idata.extend(pm.compute_log_likelihood(idata))
-
 
 with rec_tds_era_adjusted:
     idata2.extend(
-        pm.sample(nuts_sampler="numpyro", target_accept=0.99, random_seed=rng)
+        pm.sample(nuts_sampler="numpyro", random_seed=rng, target_accept=0.99)
     )
-    idata2.extend(pm.compute_log_likelihood(idata2))
-
 
 idata2.sample_stats.diverging.sum().data
 
-## with the centered season gp we get the rhats under control
+
 az.rhat(
     idata2, var_names=["basis_coeffs_season", "basis_coeffs_games"]
 ).max().to_pandas().round(2)
 
 
-## The effective sample sizes are looking okay
-## I would love if we had larger ESS's for delta sig and touchdown sd
-## but otherwise everything else looks good
 az.ess(idata2).min().to_pandas().sort_values().round()
+
+sampler_stats = pl.from_pandas(az.summary(idata2).reset_index())
+
+sampler_stats.write_parquet("rec-tds-posterior/diagnostic-summaries.parquet")
 
 mods = ["Sans Era", "Era Adjusted"]
 
 mods_dict = dict(zip(mods, [idata, idata2]))
 
+## functionally there is not a ton of difference
+## I would probably add the era just because the difference between
+## eras is functionally really important
 az.compare(mods_dict)
+
+### realized that maybe this model relies a bit to much on information from
+## the current matchup
+## We can make at least a defenisble argument that we more or less know the weather prior to them game
+## What we want to do is adjust for how much better the defense is than the offense going into the game.
+## so
+
+cumulative_stats = (
+    agg_full_seasons.sort(["defteam", "season", "week"])
+    .with_columns(
+        pl.col("total_def_epa_game")
+        .cum_sum()
+        .over(["defteam", "season"])
+        .shift(1)
+        .alias("cumulative_def_epa"),
+        pl.col("game_id")
+        .cum_count()
+        .over(["defteam", "season"])
+        .alias("total_games_played_def"),
+    )
+    .with_columns(
+        (pl.col("cumulative_def_epa") / pl.col("total_games_played_def")).alias(
+            "def_epa_per_game"
+        )
+    )
+    .sort(["posteam", "season", "week"])
+    .with_columns(
+        pl.col("total_off_epa_game")
+        .cum_sum()
+        .over(["posteam", "season"])
+        .shift(1)
+        .alias("cumulative_off_epa"),
+        pl.col("game_id")
+        .cum_count()
+        .over(["posteam", "season"])
+        .alias("total_games_played_offense"),
+    )
+    .with_columns(
+        (pl.col("cumulative_off_epa") / pl.col("total_games_played_offense")).alias(
+            "off_epa_per_game"
+        )
+    )
+    .with_columns(
+        (pl.col("cumulative_def_epa") - pl.col("cumulative_off_epa")).alias(
+            "def_epa_diff"
+        ),  # going into the game how much better is the defense playing than the offense
+        pl.col("game_id")
+        .cum_count()
+        .over(["receiver_full_name", "season"])
+        .alias("games_played"),
+    )
+    .join(player_exp, left_on=["receiver_player_id"], right_on="gsis_id", how="left")
+    .with_columns(
+        (pl.col("season") - pl.col("rookie_season")).alias("number_of_seasons_played"),
+        pl.col("birth_date").str.to_date().dt.year().alias("birth_year"),
+    )
+    .with_columns(
+        (pl.col("season") - pl.col("birth_year")).alias("age"),
+        pl.when(pl.col("roof") == "indoors").then(1).otherwise(0).alias("is_indoors"),
+    )
+    .with_columns(
+        pl.when(pl.col("posteam") == pl.col("home_team"))
+        .then(pl.col("home_score"))
+        .otherwise(pl.col("away_score"))
+        .alias("player_team_score"),
+        pl.when(pl.col("defteam") == pl.col("away_team"))
+        .then(pl.col("away_score"))
+        .otherwise(pl.col("home_score"))
+        .alias("opponent_score"),
+        pl.when(pl.col("posteam") == pl.col("home_team"))
+        .then(pl.col("home_rest"))
+        .otherwise(pl.col("away_rest"))
+        .alias("player_rest"),
+        pl.when(pl.col("defteam") == pl.col("away_team"))
+        .then(pl.col("away_rest"))
+        .otherwise(pl.col("home_rest"))
+        .alias("opponent_rest"),
+        pl.when(pl.col("posteam") == pl.col("home_team"))
+        .then(1)
+        .otherwise(0)
+        .alias("home_game"),
+    )
+    .sort(["posteam", "season", "week", "receiver_full_name"])
+    .with_columns(
+        (pl.col("player_rest") - pl.col("opponent_rest")).alias("player_rest_diff"),
+        (pl.col("opponent_rest") - pl.col("player_rest")).alias("opponent_rest_diff"),
+    )
+    .sort(["receiver_full_name", "season", "game_id"])
+    .fill_null(0)
+)
+
+
+factors_numeric3 = [
+    "player_rest_diff",
+    "def_epa_diff",
+    "wind",
+    "temp",
+    "total_line",
+    "avg_depth_of_target",
+]
+
+factors3 = factors_numeric3 + ["div_game", "home_game", "is_indoors"]
+
+factors_numeric_train3 = cumulative_stats.select(pl.col(factors3))
+
+means = factors_numeric_train3.select(
+    [pl.col(c).mean().alias(c) for c in factors_numeric3]
+)
+sds = factors_numeric_train3.select(
+    [pl.col(c).std().alias(c) for c in factors_numeric3]
+)
+
+factors_numeric_sdz3 = factors_numeric_train3.with_columns(
+    [((pl.col(c) - means[0, c]) / sds[0, c]).alias(c) for c in factors_numeric3]
+).with_columns(
+    pl.Series("home_game", cumulative_stats["home_game"]),
+    pl.Series("div_game", cumulative_stats["div_game"]),
+    pl.Series("is_indoors", cumulative_stats["is_indoors"]),
+)
+
+cumulative_stats_pd = cumulative_stats.to_pandas()
+
+
+unique_games = cumulative_stats_pd["games_played"].sort_values().unique()
+unique_seasons = cumulative_stats_pd["number_of_seasons_played"].sort_values().unique()
+
+off_play_caller = cumulative_stats_pd["off_play_caller"].sort_values().unique()
+def_play_caller = cumulative_stats_pd["def_play_caller"].sort_values().unique()
+
+unique_players = cumulative_stats_pd["receiver_full_name"].sort_values().unique()
+
+cumulative_stats.group_by(["rec_tds"]).agg(pl.len())
+
+
+player_idx = pd.Categorical(
+    cumulative_stats_pd["receiver_full_name"], categories=unique_players
+).codes
+
+games_idx = pd.Categorical(
+    cumulative_stats_pd["games_played"], categories=unique_games
+).codes
+
+off_play_caller_idx = pd.Categorical(
+    cumulative_stats_pd["off_play_caller"], categories=off_play_caller
+).codes
+
+def_play_caller_idx = pd.Categorical(
+    cumulative_stats_pd["def_play_caller"], categories=def_play_caller
+).codes
+
+coords3 = {
+    "factors": factors3,
+    "gameday": unique_games,
+    "seasons": unique_seasons,
+    "obs_id": cumulative_stats_pd.index,
+    "player": unique_players,
+    "off_play_caller": off_play_caller,
+    "def_play_caller": def_play_caller,
+    "time_scale": ["games", "season"],
+}
+
+empirical_probs = cumulative_stats_pd["rec_tds"].value_counts(normalize=True).to_numpy()
+
+cumulative_probs = empirical_probs.cumsum()[:-1]
+
+cutpoints_standard = norm.ppf(cumulative_probs)
+
+delta_prior = np.diff(cutpoints_standard)
+
+with pm.Model(coords=coords3) as rec_tds_era_adjusted:
+    factor_data = pm.Data(
+        "factor_data", factors_numeric_sdz3, dims=("obs_id", "factor")
+    )
+    games_id = pm.Data("games_id", games_idx, dims="obs_id")
+    player_id = pm.Data("player_id", player_idx, dims="obs_id")
+    season_id = pm.Data(
+        "season_id",
+        cumulative_stats["number_of_seasons_played"].to_numpy(),
+        dims="obs_id",
+    )
+
+    rec_tds_obs = pm.Data(
+        "rec_tds_obs", cumulative_stats["rec_tds"].to_numpy(), dims="obs_id"
+    )
+
+    x_gamedays = pm.Data("x_gamedays", unique_games, dims="gameday")[:, None]
+    x_seasons = pm.Data("x_seasons", unique_seasons, dims="seasons")[:, None]
+
+    # ref notebook sets it at the max of goals scored of the games so we are going to do the same
+    intercept_sigma = 4
+    sd = touchdown_dist.to_pymc("touchdown_sd")
+
+    baseline_sigma = pt.sqrt(intercept_sigma**2 + sd**2 / len(coords3["player"]))
+
+    baseline = baseline_sigma * pm.Normal("baseline")
+
+    player_effect = pm.Deterministic(
+        "player_effect",
+        baseline + pm.ZeroSumNormal("player_effect_raw", sigma=sd, dims="player"),
+        dims="player",
+    )
+
+    # bumbing this up a bit
+    alpha_scale, upper_scale = 0.021, 2.0
+    gps_sigma = pm.Exponential(
+        "gps_sigma", lam=-np.log(alpha_scale) / upper_scale, dims="time_scale"
+    )
+
+    ls = pm.InverseGamma(
+        "ls",
+        alpha=np.array([short_term_form.alpha, seasons_gp_prior.alpha]),
+        beta=np.array([short_term_form.beta, seasons_gp_prior.beta]),
+        dims="time_scale",
+    )
+
+    cov_games = gps_sigma[0] ** 2 * pm.gp.cov.Matern52(input_dim=1, ls=ls[0])
+    cov_seasons = gps_sigma[1] ** 2 * pm.gp.cov.Matern52(input_dim=1, ls=ls[1])
+
+    gp_games = pm.gp.HSGP(m=[within_m], c=within_c, cov_func=cov_games)
+    gp_season = pm.gp.HSGP(
+        m=[seasons_m], c=seasons_c, cov_func=cov_seasons, parametrization="centered"
+    )
+
+    basis_vectors_game, sqrt_psd_game = gp_games.prior_linearized(X=x_gamedays)
+
+    basis_coeffs_games = pm.Normal("basis_coeffs_games", shape=gp_games.n_basis_vectors)
+
+    f_games = pm.Deterministic(
+        "f_games",
+        basis_vectors_game @ (basis_coeffs_games * sqrt_psd_game),
+        dims="gameday",
+    )
+
+    basis_vectors_season, sqrt_psd_season = gp_season.prior_linearized(X=x_seasons)
+
+    basis_coeffs_season = pm.Normal(
+        "basis_coeffs_season", shape=gp_season.n_basis_vectors
+    )
+
+    f_season = pm.Deterministic(
+        "f_season",
+        basis_vectors_season @ (basis_coeffs_season * sqrt_psd_season),
+        dims="seasons",
+    )
+
+    alpha = pm.Deterministic(
+        "alpha",
+        player_effect[player_id] + f_season[season_id] + f_games[games_id],
+        dims="obs_id",
+    )
+    slope = pm.Normal("slope", sigma=0.25, dims="factors")
+
+    eta = pm.Deterministic(
+        "eta", alpha + pm.math.dot(factor_data, slope), dims="obs_id"
+    )
+    cutpoints_off = 4
+
+    delta_mean = pm.Normal(
+        "delta_mean", mu=delta_prior * cutpoints_off, sigma=1, shape=2
+    )
+
+    delta_sig = pm.Exponential("delta_sig", 1, shape=2)
+
+    player_delta = delta_mean + delta_sig * pm.Normal(
+        "player_delta", shape=(len(coords3["player"]), 2)
+    )
+
+    cutpoints = pm.Deterministic(
+        "cutpoints",
+        pt.concatenate(
+            [
+                pt.full((player_effect.shape[0], 1), cutpoints_off),
+                pt.cumsum(pt.softplus(player_delta), axis=-1) + cutpoints_off,
+            ],
+            axis=-1,
+        ),
+    )
+
+    pm.OrderedLogistic(
+        "tds_scored",
+        cutpoints=cutpoints[player_id],
+        eta=eta,
+        observed=rec_tds_obs,
+        dims="obs_id",
+    )
+
+
+with rec_tds_era_adjusted:
+    idata3 = pm.sample_prior_predictive()
+
+
+with rec_tds_era_adjusted:
+    idata3.extend(
+        pm.sample(nuts_sampler="numpyro", random_seed=rng, target_accept=0.99)
+    )
+
+idata3.sample_stats.diverging.sum().data
+
+
+az.rhat(
+    idata3, var_names=["basis_coeffs_season", "basis_coeffs_games"]
+).max().to_pandas().round(2)
+
+
+az.ess(idata3).min().to_pandas().sort_values().round()
+
+az.plot_energy(idata3)
